bcffae21aef15598d0048124bf819f83f26d0925
Phoenix-Firas <106271549+Phoenix-Firas@users.noreply.github.com>
2022-09-23 16:32:17 +0300

    implemented getting data though jquery

--- app.js
+++ app.js
@@ -1,19 +1,27 @@
 const news_body = document.getElementById('news_body');
 
-function renderNews(headline_txt, author_txt, article_txt, date_txt){
+function renderNews(response){
+    for(data of response){
     let headline = document.createElement('h2');
-    let author = document.createElement('h3');
+    let author = document.createElement('h5');
     let article = document.createElement('p');
-    let date = document.createElement('h5');
+    let date = document.createElement('h6');
 
-    headline.innerText = headline_txt;
-    author.innerText = author_txt;
-    article.innerText = article_txt;
-    date.innerText = date_txt;
+    headline.innerHTML = data.headline;
+    author.innerText = data.author;
+    article.innerText = data.article;
+    date.innerText = data.date;
+    
+    news_body.append(headline, author, article, date);
 
-    news_body.append(headline, author, article, date)
+    };
+};
 
-}
+
+$.getJSON('http://localhost/myprograms/news-api.php',
+function(response){
+    renderNews(response);
+});
 
 
 
